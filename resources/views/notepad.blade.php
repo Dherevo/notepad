c319ebb867e20aa1e0616ffe8e2f22b69a446c52
Mateu <arbo.mateu@gmail.com>
2023-01-14 16:08:36 +0100

    CSS Styles

--- resources/views/notepad.blade.php
+++ resources/views/notepad.blade.php
@@ -14,14 +14,14 @@
 
 </head>
 <body class="antialiased">
-<div
-    class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+<div id="content"
+    class="relative items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
 
     <div class="sm:px-6 lg:px-8">
         <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
             <div class="grid grid-cols-1 md:grid-cols-1">
                 <header>Notepad</header>
-                <section id="content" class="p-6">
+                <section class="p-6">
 
                     <div id="search">
                         <input type="text" onkeyup="searchNote(this.value)" placeholder="Search note...">
